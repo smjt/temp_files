09d026af014b8ebbc48ac0bd1faa058bd7a52f38
Soumyajit <smjt@Soumyajits-MacBook-Pro.local>
2019-07-31 12:26:13 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello, I am a template."
+
+}
+
+resource "null_resource" "sleep" {
+  count=2000
+  triggers {
+    uuid = "${uuid()}"
+  }
+
+  provisioner "local-exec" {
+    command = "mktemp -d ${count.index}"
+  }
+}
